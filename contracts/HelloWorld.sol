f5e14c1d8eccd3961ac46b433da67414867dfc63
haidar <89732397+haidarhamda@users.noreply.github.com>
2024-12-28 15:41:38 +0700

    init

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+//pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.4.25;
+contract HelloWorld {
+  // Define a string variable
+  string name;
+//  constructor() public {
+//  }
+
+  // Modifies the value of string variable (name)
+  function inputUser(string _name) public {
+    name = _name;
+  }
+
+  // Validated that the string variable (name) holds the updated value
+  function dispUser() public view returns(string){
+    return (append("Hello world! My name is ", name));
+  }
+
+  // Appends 2 strings a, b
+  function append(string a, string b) internal pure returns (string) {
+    return string(abi.encodePacked(a, b));
+  }
+}
